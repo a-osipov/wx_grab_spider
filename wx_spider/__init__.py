c44f9774898bb3bbd7e084f58c5f5f994bb25426
a-osipov <nkosmo67@gmail.com>
2021-05-29 09:27:45 +0700

    initial

---
+++ wx_spider/__init__.py
@@ -0,0 +1,6 @@
+from wx_spider.base import WxSpider, WxSpiderThread, LogginSpiderView, text2html, LOGGING_STYLES
+from wx_spider.controls import types, ProtoControl
+from wx_spider.pref import PreferencesBase
+from wx_spider.template import Template
+
+# from wx_spider.pref import get_spider_preference, set_preferences
